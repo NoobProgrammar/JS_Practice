ab90b932617304892d9286d58deefc1e373c8ed6
NoobProgrammar <168478840+NoobProgrammar@users.noreply.github.com>
2024-07-24 10:29:10 +0000

    data types

--- revision.js
+++ revision.js
@@ -1,2 +1,15 @@
 console.log("Nice to see you again!!!");
-console.table(["never", "thought", "we", "would meet", "again"])
+console.table(["never", "thought", "we", "would meet", "again"]);
+
+const constent = 12345;
+let userName = "YourName";
+var variable = "Not recomended";
+general = "String123";
+stare = true;
+let number = BigInt(99999999999999)
+let nothing;
+nobody = null
+
+
+console.table([typeof constent,typeof userName,typeof stare,typeof number, typeof nothing, typeof nobody]);
+
