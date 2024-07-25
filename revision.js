928ae45b94083a57c67ed77925451c1b4f006bff
NoobProgrammar <168478840+NoobProgrammar@users.noreply.github.com>
2024-07-25 11:23:26 +0000

    nothing

--- revision.js
+++ revision.js
@@ -1,5 +1,5 @@
 console.log("Nice to see you again!!!");
-console.table(["never", "thought", "we", "would meet", "again"]);
+// console.table(["never", "thought", "we", "would meet", "again"]);
 
 const constent = 12345;
 let userName = "YourName";
@@ -10,6 +10,20 @@ let number = BigInt(99999999999999)
 let nothing;
 nobody = null
 
+const sym1 = Symbol("fool")
+const sym2 = Symbol("fool")
 
-console.table([typeof constent,typeof userName,typeof stare,typeof number, typeof nothing, typeof nobody]);
+console.log(sym1 === sym2);
 
+console.table([typeof constent,typeof userName,typeof stare,typeof number, typeof nothing, typeof nobody, typeof sym]);
+
+let heros = ["superman","thor"]
+let object = {
+    name: "yourName",
+    age: 18,
+    gender: "male"
+}
+
+let myFunction = function () {
+    console.log("Just a function!!!");
+}
