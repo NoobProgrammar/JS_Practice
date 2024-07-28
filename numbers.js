e8583f0537f3d3a9bcf43f0e89df40e24d0fa59f
NoobProgrammar <168478840+NoobProgrammar@users.noreply.github.com>
2024-07-28 15:10:47 +0000

    numbers

---
+++ numbers.js
@@ -0,0 +1,7 @@
+const num = 400
+
+let number = new Number(1000)
+
+// console.log(num.toString().length);
+// console.log(number.toFixed(2));
+
--- string.js
+++ string.js
@@ -5,37 +5,37 @@ console.log(`Hello, my name is ${userName}. I'm ${age} years old.`);
 
 const superHero = "Iron Man "
 
-// console.log(superHero[2]);
+console.log(superHero[2]);
 
-// console.log(superHero.length);
+console.log(superHero.length);
 
-// console.log(superHero.toUpperCase());
+console.log(superHero.toUpperCase());
 
-// console.log(superHero.charAt(3));
-// console.log(superHero.indexOf('n')); /* Its case sensitive */
+console.log(superHero.charAt(3));
+console.log(superHero.indexOf('n')); /* Its case sensitive */
 
-// const newHero = superHero.substring(0, 4)
-// console.log(newHero);
+const newHero = superHero.substring(0, 4)
+console.log(newHero);
 
-// const newSuperHero = superHero.slice(0, 4)
-// console.log(newSuperHero);
+const newSuperHero = superHero.slice(0, 4)
+console.log(newSuperHero);
 
-// const trimming = "        hello          ";
-// console.log(trimming.trim());
+const trimming = "        hello          ";
+console.log(trimming.trim());
 
-// let url = "https://yournamexyz.com/give%20your%20name"
-// console.log(url.replace("%20", "-"));
+let url = "https://yournamexyz.com/give%20your%20name"
+console.log(url.replace("%20", "-"));
 
-// console.log(url.includes('yourname'));
-// console.log(url.split('/'));
+console.log(url.includes('yourname'));
+console.log(url.split('/'));
 
-// console.log(superHero.charCodeAt(3)); //utf-16 code units
+console.log(superHero.charCodeAt(3)); //utf-16 code units
 
-// const cat1 = "tiger"
-// const cat2 = "lion"
-// console.log(cat1.concat(" and ",cat2));
+const cat1 = "tiger"
+const cat2 = "lion"
+console.log(cat1.concat(" and ",cat2));
 
-// let statement = "I here by declare this land belongs to the government of xyz"
-// console.log(statement.lastIndexOf("land"));
+let statement = "I here by declare this land belongs to the government of xyz"
+console.log(statement.lastIndexOf("land"));
 
 
