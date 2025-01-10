1bbafb639c67ba9263e59c8f246435ddcb61e266
NoobProgrammar <168478840+NoobProgrammar@users.noreply.github.com>
2025-01-10 11:13:57 +0000

    scopes end

--- scopes.js
+++ scopes.js
@@ -5,9 +5,34 @@ if (true) {
     const b = 100
     var c = 1000
 }
-
 // console.log(a);
 // console.log(b);
 // console.log(c);
 // var can get out of scope
 
+function one() {
+    const var1 = 11
+
+    function two() {
+        const var2 =22
+        // console.log(var1);
+    }
+    // console.log(var2);
+    two();
+}
+
+one();
+
+// similar with if else statements
+
+console.log(addOne(5)) //this type of functions can be called before
+function addOne(num) {
+    return num + 1;
+}
+
+
+const addFive = function (num){
+    return num + 2;
+}
+
+console.log(addFive(11)) //this type of functions can not be called before
