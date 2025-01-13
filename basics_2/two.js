d3157eb90edd61287983dd72c714ac377f7faa62
NoobProgrammar <168478840+NoobProgrammar@users.noreply.github.com>
2025-01-13 06:17:05 +0000

    break & continue

--- basics_2/two.js
+++ basics_2/two.js
@@ -3,9 +3,9 @@ const array = [1, 11, 111, 1111, 11111, 111111]
 for (let ijk = 0; ijk < array.length; ijk++) {
     const element = array[ijk];
     if (element === 111) {
-        // console.log("its 111");
+        console.log("its 111");
     }
-    // console.log(element);
+    console.log(element);
 }
 
 for (let i = 10; i >= 0; i--) {
@@ -15,3 +15,32 @@ for (let i = 10; i >= 0; i--) {
     }
 }
 
+for (let i = 1; i <= 10; i++) {
+    if (i == 5) {
+        break;
+    }
+    console.log(i);
+}
+console.log();
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+    if (i == 5) {
+        break;
+    }
+}
+
+for (let i = 0; i <= 10; i++) {
+    if (i == 5) {
+        console.log("its 5");
+        continue;
+    }
+    console.log(i);
+}
+console.log();
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+    if (i == 5) {
+        console.log("its 5");
+        continue;
+    }
+}
